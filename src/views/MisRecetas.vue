d2c91ee6aab399e33a6bd19638a4b9a359df5163
Luis Gerardo <39955366+luis3698@users.noreply.github.com>
2024-01-02 23:03:57 -0500

    mis recetas

--- src/views/MisRecetas.vue
+++ src/views/MisRecetas.vue
@@ -7,10 +7,10 @@
     <p v-if="databaseStore.loadingDoc">Loading docs...</p>
 
     <a-space direction="vertical" v-if="!databaseStore.loadingDoc" style="width: 100%;">
-      <a-card v-for="data in userRecipes" :key="data.id" :title="data.name">
+      <a-card v-for="data in userRecipes" :key="data.id" :title="data.name" class="recipe-card">
         <template #extra>
           <a-space>
-            <a-popconfirm title="¿Estas seguro de eliminarlo?" ok-text="Si" cancel-text="No" @confirm="confirm(data.id)" @cancel="cancel">
+            <a-popconfirm title="¿Estás seguro de eliminarlo?" ok-text="Sí" cancel-text="No" @confirm="confirm(data.id)" @cancel="cancel">
               <a-button danger :loading="databaseStore.loading" :disabled="databaseStore.loading" shape="circle">
                 <template #icon>
                   <DeleteOutlined/>
@@ -30,13 +30,13 @@
           </a-space>
         </template>
         <p>{{ data.descripcionR }}</p>
-        <p >Ingredientes</p>
+        <p>Ingredientes</p>
         <a-row :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }">
           <a-col :span="24" :sm="12" :md="8" :lg="6" v-for="ingrediente in data.ingredientes" :key="ingrediente">
             <a-tag>{{ ingrediente }}</a-tag>
           </a-col>
         </a-row>
-        <img :src="data.imageUrl" alt="Imagen de la receta" style="max-width: 100%; margin-top: 10px;">
+        <img :src="data.imageUrl" alt="Imagen de la receta" class="recipe-image">
       </a-card> 
     </a-space>
   </div>
@@ -90,3 +90,30 @@ const copiarPortapeles = async (id, descripcionR, ingredientes) => {
   }
 };
 </script>
+<style scoped>
+/* Estilos adicionales para las tarjetas de recetas */
+.recipe-card {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease-in-out;
+  box-sizing: border-box;
+  overflow: hidden; /* Evita que el contenido sobresalga */
+  height: 100%; /* Hace que las tarjetas tengan la misma altura */
+}
+
+.recipe-card:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+/* Ajuste de margen inferior para las tarjetas */
+.recipe-card:not(:last-child) {
+  margin-bottom: 16px;
+}
+
+/* Estilos para las imágenes de recetas */
+.recipe-image {
+  width: 200px; /* Tamaño predeterminado de ancho */
+  height: 200px; /* Tamaño predeterminado de alto */
+  object-fit: cover; /* Ajusta el tamaño de la imagen sin distorsionarla */
+  margin-top: 10px;
+}
+</style>
